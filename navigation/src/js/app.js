adf4b87fddbed59b3e06fcb11ad9a82bdadb1013
Ashton Lance <ashton@simplefocus.com>
2018-09-05 14:10:04 -0400

    Updated assests / smoothing of transitions

--- navigation/src/js/app.js
+++ navigation/src/js/app.js
@@ -1,2 +1,13 @@
 require('../css/app.css');
 
+$(document).ready(function () {
+    $('.nav-toggle').on('click', function () {
+        $('nav').toggleClass('active');
+    });
+    $('ul.navigation li a').on('click', function () {
+        $('nav').addClass('active');
+        $("ul.navigation li a.active-item").removeClass("active-item");
+        console.log($(this))
+        $(this).addClass("active-item");
+    })
+});
