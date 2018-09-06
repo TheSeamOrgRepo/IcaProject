d49ce7914bad8648926e006d2f0db1d4225f3f6a
Ashton Lance <ashton@simplefocus.com>
2018-09-06 16:27:18 -0400

    Build files

--- navigation/build/js/app.js
+++ navigation/build/js/app.js
@@ -1,28 +1,54 @@
-$(document).ready(function () {
-    $('.logo-wrapper').on('click', function () {
-        $("ul.navigation li a.active-item").removeClass("active-item");
-        $('nav').removeClass('active');
-    });
-    $('ul.navigation li a').on('click', function () {
-        $('nav').addClass('active');
-        $("ul.navigation li a.active-item").removeClass("active-item");
-        $(this).addClass("active-item");
-    });
+require('../css/app.css');
 
+document.addEventListener('DOMContentLoaded', function () {
+    var logoWrapper = document.getElementsByClassName('logo-wrapper')[0];
+    var navLink = document.getElementsByClassName('nav-link');
+    var navMenu = document.getElementsByClassName('navigation');
+    var activeNavItem = document.getElementsByClassName('active-item');
+    var sideBar = document.querySelectorAll('nav')[0];
     var timer;
     var delay = 100;
-    $('nav').hover(
-        function () {
-            timer = setTimeout(function () {
-                if ($('nav').hasClass('active')) {
-                    $('nav').removeClass('active');
-                }
-            }, delay);
-        },
-        function () {
-            clearTimeout(timer);
-            if (!$('nav').hasClass('active') && $("ul.navigation li a.active-item").hasClass('active-item')) {
-                $('nav').addClass('active');
+
+    //Retun to open state when returning to main dashboard
+    logoWrapper.addEventListener('click', function () {
+        for (var i = 0; i < activeNavItem.length; i++) {
+            activeNavItem[i].classList.remove('active-item');
+        }
+        sideBar.classList.remove('active');
+    });
+
+    //Add active-item class to current link and collapse sidebar
+    [].forEach.call(navLink, function (el) {
+        el.addEventListener('click', function () {
+            sideBar.classList.add('active')
+            for (var i = 0; i < activeNavItem.length; i++) {
+                activeNavItem[i].classList.remove('active-item');
             }
-        });
+            el.classList.add('active-item')
+        })
+    })
+
+    //Reveals sidebar on hover
+    function toggleNav() {
+        if (sideBar.classList.contains('active')) {
+            var that = this;
+            timer = setTimeout(function () {
+                that.classList.toggle('active');
+            }, delay);
+        }
+    }
+
+    //Resets the settimeout from the mouseenter event and will return the menu to the
+    //collapsed position if there is an active-item link
+    function toggleNavReset() {
+        clearTimeout(timer)
+        if (!sideBar.classList.contains('active') && activeNavItem.length > 0) {
+            sideBar.classList.add('active')
+        }
+    };
+
+    //Add eventlisteners to sidebar for hover-on and hover-off events
+    sideBar.addEventListener('mouseenter', toggleNav)
+    sideBar.addEventListener('mouseleave', toggleNavReset)
+
 });
