4839596a33adf546121a31d10461482cf03e2a78
Ashton Lance <ashton@simplefocus.com>
2018-09-06 10:09:13 -0400

    Smoothness

--- navigation/src/js/app.js
+++ navigation/src/js/app.js
@@ -2,12 +2,28 @@ require('../css/app.css');
 
 $(document).ready(function () {
     $('.nav-toggle').on('click', function () {
-        $('nav').toggleClass('active');
+        $('nav').addClass('active');
     });
     $('ul.navigation li a').on('click', function () {
         $('nav').addClass('active');
         $("ul.navigation li a.active-item").removeClass("active-item");
-        console.log($(this))
         $(this).addClass("active-item");
-    })
+    });
+
+    var timer;
+    var delay = 100;
+    $('ul.navigation').hover(
+        function () {
+            timer = setTimeout(function () {
+                if ($('nav').hasClass('active')) {
+                    $('nav').removeClass('active');
+                }
+            }, delay);
+        },
+        function () {
+            clearTimeout(timer);
+            if (!$('nav').hasClass('active') && $("ul.navigation li a.active-item").hasClass('active-item')) {
+                $('nav').addClass('active');
+            }
+        });
 });
